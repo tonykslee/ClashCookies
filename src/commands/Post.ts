d005b8010c785d001764c77db05d358368e3efe8
Tony Lee <18295450+tonykslee@users.noreply.github.com>
2026-02-21 18:19:24 -0800

    revert(post): restore sync-time modal title to Post Sync Time (#47)

--- src/commands/Post.ts
+++ src/commands/Post.ts
@@ -411,7 +411,7 @@ export const Post: Command = {
 
     const modal = new ModalBuilder()
       .setCustomId(buildModalCustomId(interaction.user.id))
-      .setTitle("Do not post outside FWA Rep channel");
+      .setTitle("Post Sync Time");
 
     const dateInput = new TextInputBuilder()
       .setCustomId(DATE_INPUT_ID)
